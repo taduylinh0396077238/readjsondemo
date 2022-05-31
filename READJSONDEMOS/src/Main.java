fa8dd125f59843cbbc50cbe97b333dfb09909bfe
Duy Linh <cudotn111@gmail.com>
2022-05-31 11:57:30 +0700

    Duy Linh

---
+++ READJSONDEMOS/src/Main.java
@@ -0,0 +1,33 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
+        try{
+            Object obj = parser.parse(new FileReader(
+                    "course.json"));
+            JSONObject jsonObject = (JSONObject)obj;
+            String name = (String)jsonObject.get("Name");
+            String course = (String)jsonObject.get("Course");
+            JSONArray subjects = (JSONArray)jsonObject.get("Subjects");
+            System.out.println("Name" + name);
+            System.out.println("Course" + course);
+            System.out.println("Subjects" );
+            Iterator iterator = subjects.iterator();
+            while (iterator.hasNext()){
+                System.out.println(iterator.next());
+            }
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
